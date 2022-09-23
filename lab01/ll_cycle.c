04bfeca637ace66ba969a284ae44eaaa0ea94aa6
username-31 <85339089+username-31@users.noreply.github.com>
2022-09-24 00:48:11 +0800

    lab01 done, Ex4 done in Ubuntu

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *hare = head, *turtle = head;
+    while (hare != NULL)
+    {
+        hare = hare->next;
+        if (hare == NULL) break;
+        hare = hare->next;
+        if (hare == NULL) break;
+        turtle = turtle->next;                                                                                                     
+        if (hare == turtle)
+        {
+            return 1;
+        }
+    }
     return 0;
 }
